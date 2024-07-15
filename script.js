7d42f02754d5edb070fec2d2caf6f3a29f57b682
RyzenMu <muruga13in2022@gmail.com>
2024-07-15 20:55:54 +0530

    classList.remove

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ const btn2 = document.querySelector('.b2');
 const btn3 = document.querySelector('.b3');
 
 btn1.addEventListener('click', function(){
-    document.querySelector(".p1").classList.add("vis");
+    document.querySelector(".p1").classList.remove("hidden");
     document.querySelector(".p2").classList.add("hidden");
     document.querySelector(".p3").classList.add("hidden");
 
 });
 
 btn2.addEventListener('click', function(){
-    document.querySelector(".p2").classList.add("vis");
+    document.querySelector(".p2").classList.remove("hidden");
     document.querySelector(".p1").classList.add("hidden");
     document.querySelector(".p3").classList.add("hidden");
 });
 
 btn3.addEventListener('click', function(){
-    document.querySelector(".p3").classList.add("vis");
+    document.querySelector(".p3").classList.remove("hidden");
     document.querySelector(".p1").classList.add("hidden");
     document.querySelector(".p2").classList.add("hidden");
 });
